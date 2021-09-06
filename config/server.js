d019f7e9d605b568a219415e40de72ae525e0443
Mohsin Ali <55811423+mosin92@users.noreply.github.com>
2021-09-06 10:11:11 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '23f8bb723ae96303403fe8bb17c1bf23'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '25b7bfde-e658-4990-98cb-2cb6e9a7f099'
+};
